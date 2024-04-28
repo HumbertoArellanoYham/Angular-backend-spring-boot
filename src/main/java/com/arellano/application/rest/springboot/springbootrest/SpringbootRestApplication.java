d328ccf5243ba6381a57923f8e50a99c071b48b8
HumbertoArellano <humbertoyham25@gmail.com>
2024-04-28 12:13:20 -0700

    Problem solution

--- src/main/java/com/arellano/application/rest/springboot/springbootrest/SpringbootRestApplication.java
+++ src/main/java/com/arellano/application/rest/springboot/springbootrest/SpringbootRestApplication.java
@@ -2,28 +2,11 @@ package com.arellano.application.rest.springboot.springbootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringbootRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootRestApplication.class, args);
-	}
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-            }
-        };
 	}
 }
